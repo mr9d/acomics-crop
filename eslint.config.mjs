5077674240681b78d7605b5b233f1b44f376562e
Aleksandr Kozlov <themrgd@gmail.com>
2025-05-27 23:06:04 +0100

    Fix eslint for node files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,19 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser
+    },
+    overrides: [
+      {
+        files: ["release.js", "webpack.config.js"],
+        languageOptions: {
+          globals: globals.node
+        }
+      }
+    ]
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
@@ -14,5 +26,5 @@ export default [
       indent: ["error", 2],
       eqeqeq: ["error", "always"],
     }
-  }
+  },
 ];
